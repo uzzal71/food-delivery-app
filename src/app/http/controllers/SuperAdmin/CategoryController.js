4d82711b16f4c767b33cf3194d26960921de706d
uzzal71 <uzzalroy.acm@gmail.com>
2023-11-02 12:21:41 +0600

    category controller method

--- src/app/http/controllers/SuperAdmin/CategoryController.js
+++ src/app/http/controllers/SuperAdmin/CategoryController.js
@@ -1,3 +1,37 @@
 export class CategoryController {
-    
+     /**
+     * @objective achive all categories
+     * @method get
+     * @req parameter received
+     * @res parameter received
+     */
+    getCategories = async(req, res) => {}
+    /**
+     * @objective create a single category
+     * @method post
+     * @req parameter received
+     * @res parameter received
+     */
+    createCategory = async(req, res) => {}
+    /**
+     * @objective achine single category
+     * @method get
+     * @req parameter received
+     * @res parameter received
+     */
+    getCategory = async(req, res) => {}
+    /**
+     * @objective update a single category
+     * @method put
+     * @req parameter received
+     * @res parameter received
+     */
+    updateCategory = async(req, res) => {}
+    /**
+     * @objective delete a single category
+     * @method delete
+     * @req parameter received
+     * @res parameter received
+     */
+    destoryCategory = async(req, res) => {}
 }
--- src/app/services/SuperAdmin/CategoryService.js
+++ src/app/services/SuperAdmin/CategoryService.js
@@ -1,27 +1,7 @@
 export class CategoryService {
-    /**
-     * @objective create a single category
-     * @method post
-     */
     async createCategory() {}
-    /**
-     * @objective achive all categories
-     * @method get
-     */
     async getCategories() {}
-    /**
-     * @objective achine single category
-     * @method get
-     */
     async getCategory() {}
-    /**
-     * @objective update a single category
-     * @method put
-     */
     async updateCategory() {}
-    /**
-     * @objective delete a single category
-     * @method delete
-     */
     async destoryCategory() {}
 }
